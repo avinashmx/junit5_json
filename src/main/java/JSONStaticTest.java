87b524973b714b7b7f4b34bb74c22a01f821059a
Avinash R <>
2020-12-20 21:00:51 -0500

    Another static file demonstration

--- src/main/java/JSONStaticTest.java
+++ src/main/java/JSONStaticTest.java
@@ -3,6 +3,7 @@
  */
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 
@@ -12,6 +13,16 @@ public class JSONStaticTest {
     public void testName(ArgumentsAccessor argumentsAccessor) {
         System.out.println("Asserting on " + argumentsAccessor.getString(0));
         Assertions.assertTrue(argumentsAccessor.getString(0).length() == 3);
+    }
+
+    @Test
+    public void testDog1(){
+        Assertions.assertTrue("Dog".length()== 3);
+    }
+
+    @Test
+    public void testGod1(){
+        Assertions.assertTrue("God".length()== 3);
     }
 
 }
